1afa3dcfa992b05146e62520959f3a4e59811ca6
sarthakgarg234 <sarthakgarg234@gmail.com>
2021-06-09 19:55:12 +0530

    Spelling Changes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,7 @@
         <header>
             <nav class="navbar navbar-expand-sm navbar-light bg-light">
                 <div class="container-fluid">
-                  <a class="navbar-brand" href="/">Resto</a>
+                  <a class="navbar-brand" href="/">Restaurant</a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                   </button>
